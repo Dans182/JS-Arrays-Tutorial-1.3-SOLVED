db5e645e184eee4a3d42e4cf8158ba54a6914f67
Dans182 <dgaiteiro@gmail.com>
2022-03-31 15:15:13 +0000

    21.3 resuelto

--- exercises/21.3-Filter-list/app.js
+++ exercises/21.3-Filter-list/app.js
@@ -1,5 +1,12 @@
 let names = ['Liam','Emma','Noah','Olivia','William','Ava','James','Isabella','Logan','Sophia','Benjamin','Mia','Mason','Charlotte','Elijah','Amelia','Oliver','Evelyn','Jacob','Abigail','Lucas','Harper','Michael','Emily','Alexander','Elizabeth','Ethan','Avery','Daniel','Sofia','Matthew','Ella','Aiden','Madison','Henry','Scarlett','Joseph','Victoria','Jackson','Aria','Samuel','Grace','Sebastian','Chloe','David','Camila','Carter','Penelope','Wyatt','Riley'];
 
 //declare your function here
+function filterByName (p) {
+    return names.filter(function(el){
+        return el.toLowerCase().indexOf(p.toLowerCase()) > -1;
+    })
+}
 
-console.log(filterByName(names, 'am'));
+
+console.log(filterByName('am'));
+// console.log(filterByName(names,'am'));
