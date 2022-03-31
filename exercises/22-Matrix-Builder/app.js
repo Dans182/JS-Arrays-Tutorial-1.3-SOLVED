9093c9298c3cd537480f9288b9a4af9d363e833f
Dans182 <dgaiteiro@gmail.com>
2022-03-31 16:01:58 +0000

    ejercicio 22 resuelto

--- exercises/22-Matrix-Builder/app.js
+++ exercises/22-Matrix-Builder/app.js
@@ -1,6 +1,19 @@
 // Code goes here
-
+ 
+function matrixBuilder (argEntero){
+    let matrixFinal = []
+    for (let i = 0; i< argEntero; i++){
+        let matrix = []
+        matrixFinal.push(matrix);
+        for (let i = 0; i< argEntero; i++){
+           let numero = Math.floor(Math.random()*2);
+           matrix.push(numero);
+        }
+    }
+    return matrixFinal
+  }
 
 
 // do not change anything from this line down
-console.log(matrixBuilder(5))
+ console.log(matrixBuilder(5))
+
