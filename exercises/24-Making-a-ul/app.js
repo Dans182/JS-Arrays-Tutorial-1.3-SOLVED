576604aaa5306d7c611b0554d20a2bea8d34dfd6
Dans182 <dgaiteiro@gmail.com>
2022-04-05 14:46:24 +0000

    ejercicio 24 listo

--- exercises/24-Making-a-ul/app.js
+++ exercises/24-Making-a-ul/app.js
@@ -9,14 +9,21 @@ let allColors = [
 ];
 
 function generateLI(color){
+	let htmlString22 = '<li>'
+	let htmlString33 = '</li>'
+	return htmlString22 + color.label + htmlString33 
 	// your code here
 }
 
 function filterColors(color){
 	// your code here
+	if (color.sexy == true){
+		return color.label 
+	}
 }
 
 function generateHTMLFromArray(array){
+	
 	
 	let filteredOptions = array.filter(filterColors);
 	let LIs = filteredOptions.map(generateLI);
