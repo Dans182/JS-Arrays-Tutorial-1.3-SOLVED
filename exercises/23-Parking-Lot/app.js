e3c8a7890d3b6ca713f34ef29c416d58c0bbefbb
Dans182 <dgaiteiro@gmail.com>
2022-04-05 12:13:37 +0000

    ejercicio 23

--- exercises/23-Parking-Lot/app.js
+++ exercises/23-Parking-Lot/app.js
@@ -1,6 +1,30 @@
 let parking_state = [
-  [1,1,1],
-  [0,0,0],
-  [1,1,2]
+  [1, 1, 1],
+  [0, 0, 0],
+  [1, 1, 2]
 ]
+
+  let state = {
+
+    "availableSlots": 0,
+    "occupiedSlots": 0,
+    "totalSlots": 0,
+  }
+
+const getParkingLotState = () => {
+
+  for (let value of parking_state) {
+      for (let valor of value) {
+        if (valor == 1) {
+          state.occupiedSlots += 1
+        }
+        if (valor == 2) {
+        state.availableSlots += 1
+        }
+      }
+  }
+  state.totalSlots = state.occupiedSlots + state.availableSlots
+  return state
+}
+
 console.log(getParkingLotState(parking_state))
