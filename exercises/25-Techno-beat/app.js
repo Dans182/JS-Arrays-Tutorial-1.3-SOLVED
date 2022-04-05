f27d2a979c89512b854950767fe43e8f11481226
Dans182 <dgaiteiro@gmail.com>
2022-04-05 15:52:06 +0000

    ejercicio final casi terminado

--- exercises/25-Techno-beat/app.js
+++ exercises/25-Techno-beat/app.js
@@ -1,9 +1,26 @@
 // Add your code here
+let aux = [];
+let aux2 = [];
+const lyricsGenerator = (array) => {
+    for (let value of array) {
+        switch (value) {
+            case 0:
+                aux.push("Boom");
+                break;
+
+            case 1:
+                aux.push("Drop the base");
+                break;
+        }
+
+    }
+    return aux
+}
 
 
 // Don't change anything bellow this line
-console.log(lyricsGenerator([0,0,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,1,1,1,0,0,0]))
-console.log(lyricsGenerator([0,0,0]))
-console.log(lyricsGenerator([1,0,1]))
-console.log(lyricsGenerator([1,1,1]))
+console.log(lyricsGenerator([0, 0, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 1, 1, 1, 0, 0, 0]))
+console.log(lyricsGenerator([0, 0, 0]))
+console.log(lyricsGenerator([1, 0, 1]))
+console.log(lyricsGenerator([1, 1, 1]))
